87da6cf5ec8f80ac4b3c2f68f2e3b52193b38204
jascination <jvg@warmhq.com>
2016-11-16 17:55:40 +0100

    Added carousel + animations
    Added carousel click components and animating frames to the project

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+
+var auto = false;
+
+
 angular
 
 .module('ar', ['ngSanitize'])
 
-.controller('homeCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-    
-    $scope.message = "Hello there";
+.controller('homeCtrl', ['$scope', '$http', '$sce', '$interval', function($scope, $http, $sce, $interval) {
 
     $scope.frames = {
         w: 12,
@@ -13,9 +15,11 @@ angular
         spacebetween: 3
     };
 
-    $scope.trustSrc = function(src) {
-        return $sce.trustAsResourceUrl(src);
-    }
+    $scope.carouselPos = '0 0 0';
+
+    var animating = false,
+        currentFocusIndex = 0;
+
 
     $scope.go = function(){
         $scope.loading = true;
@@ -23,43 +27,148 @@ angular
         $http.get('http://services.realestate.com.au/services/listings/' + $scope.search)
              .then(
              (res) => {
-                console.log(res.data);
                 $scope.imgs = createImgs(res.data.images);
-                console.log($scope.imgs);
                 $scope.ready = true;
+                clickSetup();
              }, () => {
                 $scope.error = "Error loading your results. Try again."
              });
     }
 
 
-
-
     function createImgs(data){
-        var d = data
-                .filter(function(e){
+        var d = data.filter(function(e){
                     return e.server.indexOf('youtube') === -1 ? true : false; 
                 });
 
         return  d.map(function(e, index){
                     return {
                         url: 'http://127.0.0.1:8080/' + e.server + '/1010x570-fit' + e.uri,
-                        position: calcPos(d.length, index)
+                        position: calcPos(d.length, index) + ' 0 -10'
                     }
                 })
     }
 
 
-    function calcPos(total, index){
-        // Calculating where the initial carousel should be
+    function calcPos(total, index, mid){
 
-        // total = 10
-        // width = 20
-        
-        var mid = Math.round(total / 2);
+        !mid ? mid = Math.round(total / 2) : '';    
+
         var xPos = (mid - index) * ($scope.frames.w + $scope.frames.spacebetween);
-        return xPos + ' 0 -10';
+        
+        return xPos
     }
+
+
+
+
+    function clickSetup(){
+
+        AFRAME.registerComponent('cursor-listener', {
+          init: function () {
+
+            this.el.addEventListener('click', function focusCarousel() {
+                var currUrl    = this.getAttribute('src'),
+                    clickedInd = 0,
+                    midPoint   = Math.round($scope.imgs.length / 2),
+                    diffInd;
+
+                if(!animating){
+
+                    animating = true;
+
+                    $scope.imgs.forEach(function(el, ind){
+                        if(el.url === currUrl){
+
+                            clickedInd = ind;
+                            diffInd = - (midPoint - clickedInd);
+
+                        } 
+                    });
+
+                    loopImgs(diffInd);
+                }
+              
+            });
+          }
+        });
+    }
+
+
+    function loopImgs(diff){
+
+        var timesToRun = Math.abs(diff); 
+
+        if(timesToRun === 0){ animating = false; return;}
+
+        for(var i = 0; i<timesToRun; i++){
+            spliceImgs();
+
+            if(i === timesToRun -1){
+                
+                var framesDone = 0;
+
+                $scope.imgs.forEach(function(e, index){
+                    var oldPos = Number(e.position.split(' ')[0]);
+                    var newPos = calcPos($scope.imgs.length, index);
+                    
+                    animateFrame(e, oldPos, newPos, function(){
+                        framesDone++;
+                        
+                        if(framesDone === $scope.imgs.length){
+                            animating = false;
+                        }
+                    })
+                })
+
+            }
+        }
+        
+        function spliceImgs(){
+            var orphan;
+                
+            if(diff < 0){
+                orphan = $scope.imgs.pop();
+                $scope.imgs.unshift(orphan);
+            } else if(diff > 0){
+                orphan = $scope.imgs.shift();
+                $scope.imgs.push(orphan);
+            }
+
+        }
+    }
+
+
+    function animateFrame(el, pos1, pos2, cb){
+        var diff = pos2 - pos1;
+        var distancePerFrame = diff / 10;
+        var currentPos = pos1;
+
+        var promise = $interval(function(){
+            var newPos = currentPos += distancePerFrame;
+                el.position = String(newPos) + ' 0 -10';
+                currentPos = newPos;
+
+            if(newPos === pos2){
+                animating = false;
+                cb();
+                $interval.cancel(promise);
+            }
+
+        },10);
+
+    }
+
+    if(auto){
+        $scope.search = 7695087;
+        $scope.go();
+    }
+
+    
+    $scope.trustSrc = function(src) {
+        return $sce.trustAsResourceUrl(src);
+    }
+
 
 }])
 
@@ -84,16 +193,26 @@ angular
         restrict: 'E',
         replace: true,
         template: '<a-scene>' +
-                    '<a-box ' + 
-                        'ng-repeat="im in imgs track by $index" ' +
-                        'color="#fff" ' +
-                        'ng-attr-width="{{frames.w}}" ' +
-                        'ng-attr-height="{{frames.h}}" ' +
-                        'ng-attr-depth="{{frames.d}}" ' +
-                        'ng-attr-position="{{im.position}}" ' +
-                        'rotation="0 0 0" ' +
-                        'ng-attr-src="{{trustSrc(im.url)}}"' +
-                    '></a-box>' +
+                    '<a-entity class="carousel" ng-attr-position="{{carouselPos}}" >' +
+                        '<a-box ' + 
+                            'ng-repeat="im in imgs track by $index" ' +
+                            'color="#fff" ' +
+                            'ng-attr-width="{{frames.w}}" ' +
+                            'ng-attr-height="{{frames.h}}" ' +
+                            'ng-attr-depth="{{frames.d}}" ' +
+                            'ng-attr-position="{{im.position}}" ' +
+                            'rotation="0 0 0" ' +
+                            'ng-attr-src="{{trustSrc(im.url)}}" ' +
+                            'cursor-listener ' +
+                        '></a-box>' +
+                    '</a-entity>' +
+                    //'<a-entity camera mouse-cursor>' +
+                    '<a-entity ng-attr-position="{{cameraPos || \'0 0 0\'}}" >' + 
+                        '<a-camera>' +
+                            '<a-cursor color="#2E3A87"></a-cursor>' +
+                        '</a-camera>' +
+                    '</a-entity>' +
                   '</a-scene>'
     }
 }])
+
